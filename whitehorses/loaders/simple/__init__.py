84e958d229ab835336d297da7f8d8e89cac21c64
oskar <oskarsinger@gmail.com>
2017-04-11 13:56:50 -0400

    Constant loader

--- whitehorses/loaders/simple/__init__.py
+++ whitehorses/loaders/simple/__init__.py
@@ -4,4 +4,6 @@ from .gaussian import ShiftingMeanGaussianLoader
 from .periodic import FakePeriodicGaussianLoader
 from .periodic import CosineLoader
 
+from .constant import ConstantLoader
+
 from .time_series import BatchPhysiologicalTimeSeriesLoader
---
+++ whitehorses/loaders/simple/constant.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+class ConstantLoader:
+
+    def __init__(self, c):
+
+        self.c = np.array([c])[:,np.newaxis]
+        self.num_rounds = 0
+
+    def get_data(self):
+
+        self.num_rounds += 1
+
+        return c
+
+    def name(self):
+
+        return 'ConstantLoader'
+
+    def cols(self):
+
+        return 1
+
+    def rows(self):
+
+        return self.num_rounds
