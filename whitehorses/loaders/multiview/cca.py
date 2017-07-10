6831427a2c4d627bb6d777113df25427a165b56a
oskar <oskarsinger@gmail.com>
2017-07-10 15:05:15 -0400

    Fu 2016 data generator

--- whitehorses/loaders/multiview/cca.py
+++ whitehorses/loaders/multiview/cca.py
@@ -1,16 +1,21 @@
 import numpy as np
 
 from linal.svd_funcs import get_svd_power
+from linal.random import get_sparse_normal
 
 # TODO: make some util functions for generating special cases
-# TODO: cite the Francis Bach paper
+# TODO: cite the Francis Bach and Fu 2016 papers
 
-def get_Fu2016_Data():
-    pass
+def get_Fu2016_data(num_data, k, ds, lazy=True, density=0.1):
+
+    Z = get_sparse_normal(k, num_data, density=density)
+
+    return [Fu2016Loader(Z, d, lazy=lazy, density=density)
+            for d in ds]
 
 def get_easy_CCAPMLs(num_data, k, ds, lazy=True):
 
-    z = np.random.randn(k, num_data)
+    Z = np.random.randn(k, num_data)
     Psi_inits = [np.random.randn(d * 2, d)
                  for d in ds]
     Psis = [np.dot(Pi.T, Pi) for Pi in Psi_inits]
@@ -22,28 +27,50 @@ def get_easy_CCAPMLs(num_data, k, ds, lazy=True):
         mus)
     CCAPML = CCAProbabilisticModelLoader
     
-    return [CCAPML(num_data, W, Psi, mu, z, lazy=lazy)
+    return [CCAPML(W, Psi, mu, Z, lazy=lazy)
             for (W, Psi, mu) in zipped]
 
-class LambdaWeightedCCAProbabilisticModelLoader:
+class Fu2016Loader:
 
-    def __init__(self, num_data, W, Psi, mu, z, lazy=True):
-        pass
+    def __init__(self, Z, d, lazy=True, density=0.1):
+
+        self.Z = Z
+        self.d = d
+        self.lazy = lazy
+        self.density = density
+
+        (self.k, self.num_data) = self.Z.shape
+        self.data = None
+
+        if not self.lazy:
+            self._set_data()
+
+        def get_data(self):
+
+            if self.data is None:
+                self._set_data()
+
+            return self.data
+
+        def _set_data(self):
+
+            A = get_sparse_normal(d, k, density=density)
+
+            self.data = np.dot(A, self.Z).T
 
 class StaticCCAProbabilisticModelLoader:
 
-    def __init__(self, num_data, W, Psi, mu, z, lazy=True):
+    def __init__(self, W, Psi, mu, Z, lazy=True):
         
-        self.num_data = num_data
         self.W = W
         self.Psi = Psi
         self.mu = mu
-        self.z = z
+        self.Z = Z
         self.lazy = lazy
 
         self.d = self.W.shape[0]
-        self.k = self.z.shape[0]
-        self.mean = np.dot(self.W, z) + self.mu
+        (self.k, self.num_data) = self.Z.shape
+        self.mean = np.dot(self.W, Z) + self.mu
         self.sd = get_svd_power(self.Psi, 0.5)
         self.data = None
 
