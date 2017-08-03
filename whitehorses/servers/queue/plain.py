1cd724c79a32c63c186f3651a55ab4f2498d3293
oskar <oskarsinger@gmail.com>
2017-08-03 02:43:31 -0400

    Reorged centering

--- whitehorses/servers/queue/plain.py
+++ whitehorses/servers/queue/plain.py
@@ -4,27 +4,43 @@ from drrobert.stats import get_zm_uv
 
 class PlainQueue:
 
-    def __init__(self, batch_size):
+    def __init__(self, 
+        batch_size, 
+        center=False):
 
         self.batch_size = batch_size
+        self.center = center
 
         self.batch = None
+        self.sum = None
 
     def add_data(self, data):
 
         data = np.copy(data)
-         
+
         if self.batch is None:
+            self.sum = data
             self.batch = data
         elif self.batch.shape[0] < self.batch_size:
+            self.sum += data
             self.batch = np.vstack([
                 self.batch, 
                 data])
         else:
+            self.sum += data - self.batch[0,:]
             self.batch = np.vstack([
                 self.batch[1:,:],
                 data])
 
+    def get_mean(self):
+
+        return self.sum / self.batch.shape[0]
+
     def get_batch(self):
 
-        return get_zm_uv(self.batch)
+        batch = np.copy(self.batch)
+
+        if self.center:
+            batch -= self.get_mean()
+
+        return batch
