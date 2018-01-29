d0945e3ca8415734d5468c6a2cf67b56b0b38c56
oskar <oskarsinger@gmail.com>
2018-01-28 22:02:44 -0500

    fixed an import

--- whitehorses/loaders/simple/__init__.py
+++ whitehorses/loaders/simple/__init__.py
@@ -4,6 +4,8 @@ from .gaussian import ShiftingMeanGaussianLoader
 from .periodic import FakePeriodicGaussianLoader
 from .periodic import EmbeddedCosineLoader
 
+from .binary import BernoulliLoader
+
 from .constant import ConstantLoader
 
 from .time_series import BatchPhysiologicalTimeSeriesLoader
